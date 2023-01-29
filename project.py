8ff94567b818e63200c9150f4773e671dd021ee1
Barry Fardar <31126364+Bardiafa@users.noreply.github.com>
2023-01-29 22:51:51 +0330

    Update project.py

--- project.py
+++ project.py
@@ -1,6 +1,3 @@
-# Bardia Fardar
-# Student Number 4003333028
-# AI Project - Final
 
 from colorama import Fore, Style
 from random import choice
@@ -43,6 +40,27 @@ board = [[EMPTY, EMPTY, EMPTY],
          [EMPTY, EMPTY, EMPTY]]
 
 
+# Run & Welcome print with Copyright & Asking the user want to play (AI vs AI) or (Player vs AI) & capturing answer
+
+
+def run():
+    while True:
+        os.system("cls")
+        print(Fore.CYAN + "           TIC" + Fore.BLUE + "-" +
+              Fore.GREEN + "TAC" + Fore.BLUE + "-" + Fore.CYAN + "TOE" + "\nBy Bardia Fardar\n")
+        mode = input(
+            Fore.CYAN + "\n ┌─[AI vs AI or Player vs AI?]\n └──╼(1 or 2)>>> " + Fore.GREEN + "")
+        if int(mode) == 1:
+            bot()
+        else:
+            Pvbot()
+
+
+if __name__ == "__main__":
+    run()
+
+
+#
 def showboard(array):
     print(first_line)
     for a in range(len(array)):
@@ -60,11 +78,15 @@ def showboard(array):
         else:
             print(middle_line)
 
+# Clearing Board using putting EMPTY in all x's & y's
+
 
 def clearBoard(brd):
     for x, row in enumerate(brd):
         for y, col in enumerate(row):
             brd[x][y] = EMPTY
+
+# This def checks if any Player wins or not using checking states
 
 
 def winningPlayer(brd, player):
@@ -82,9 +104,13 @@ def winningPlayer(brd, player):
 
     return False
 
+# This def returns the winner to winning position
+
 
 def gameWon(brd):
     return winningPlayer(brd, XPLAYER) or winningPlayer(brd, OPLAYER)
+
+# This def shows the winner or tie position to User with timeout time
 
 
 def printResult(brd):
@@ -100,6 +126,10 @@ def printResult(brd):
         print(Fore.YELLOW + "😔 Tie No winner!" + '\n')
         time.sleep(2)
 
+# This def checks if the requested cell is empty or not
+
+# First it makes cells empty then enumerate adds a counter to an iterable and returns it in a form of enumerating object.
+
 
 def emptyCells(brd):
     emptyC = []
@@ -110,15 +140,21 @@ def emptyCells(brd):
 
     return emptyC
 
+# This def checks if the lenth of cells is full or not
+
 
 def boardFull(brd):
     if len(emptyCells(brd)) == 0:
         return True
     return False
 
+# This def sets entered user position in its cell
+
 
 def setposition(brd, x, y, player):
     brd[x][y] = player
+
+# This def defines the all moves available to use and convers to array to be entered & (try) wants the user to enter next move the it checks emptycells if its full or not , at the end it sets the position & shows scoreboard & prints alpha & beta , and if all was wrong it wants the user to enter correct number
 
 
 def playerMove(brd):
@@ -138,11 +174,13 @@ def playerMove(brd):
                 os.system("cls")
                 setposition(brd, moves[move][0], moves[move][1], XPLAYER)
                 showboard(brd)
-                print("alpha = -inf")
-                print("beta = inf")
+                print("Alpha = -infinity")
+                print("Beta = infinity")
                 e = False
         except (KeyError, ValueError):
             print(Fore.RED + "Info:: " + Fore.YELLOW + "Please Enter a Number!!")
+
+# This def set the scores for if Xplayer or OPlayer won the gamed with values of 1 , -1
 
 
 def getScore(brd):
@@ -155,16 +193,22 @@ def getScore(brd):
     else:
         return 0
 
+# This def is the MiniMax Algorithm
+
 
 def MiniMax(brd, depth, player):
     row = -1
     col = -1
     best = 0
 
+    # if we were in root position or anyone won the game it will show us the winner & row & collumn
+
     if player == XPLAYER:
         best = -inf
     else:
         best = inf
+
+     # if none it will run with setting position in [0][1] the it rerun MiniMax_AB with 1 lower depth
 
     if depth == 0 or gameWon(brd):
         return [row, col, getScore(brd)]
@@ -175,16 +219,21 @@ def MiniMax(brd, depth, player):
             score = MiniMax(brd, depth - 1, -player)
             if player == XPLAYER:
 
+                # Here if score was higher than best => best will be same as score & we go to next collumn
+
                 if score[2] > best:
                     best = score[2]
                     row = cell[0]
                     col = cell[1]
 
+                # Here if score was lower than best => best will be same as score & we go to next collumn
             else:
                 if score[2] < best:
                     best = score[2]
                     row = cell[0]
                     col = cell[1]
+
+            # Here it will put brd[0][1] = EMPTY
 
             setposition(brd, cell[0], cell[1], EMPTY)
 
@@ -194,6 +243,8 @@ def MiniMax(brd, depth, player):
         else:
             return [row, col]
 
+# This def is the MiniMax Alpha-Beta Purning Algorithm
+
 
 def MiniMax_AB(brd, depth, alpha, beta, player):
     row = -1
@@ -201,27 +252,39 @@ def MiniMax_AB(brd, depth, alpha, beta, player):
     alpha_cut = 0
     beta_cut = 0
 
+    # if we were in root position or anyone won the game it will show us the winner & row & collumn
     if depth == 0 or gameWon(brd):
         return [row, col, getScore(brd)]
 
+    # if none it will run with setting position in [0][1] the it rerun MiniMax_AB with 1 lower depth
     else:
         for cell in emptyCells(brd):
             setposition(brd, cell[0], cell[1], player)
             score = MiniMax_AB(brd, depth - 1, alpha, beta, -player)
             if player == XPLAYER:
+
+                # Here if score was higher than alpha => alpha will be same as score & we go to next collumn
+
                 if score[2] > alpha:
                     alpha = score[2]
                     row = cell[0]
                     col = cell[1]
 
             else:
+
+                # Here if score was lower than beta => beta will be same as score & we go to next collumn
+
                 if score[2] < beta:
                     beta = score[2]
                     row = cell[0]
                     col = cell[1]
 
+            # Here it will put brd[0][1] = EMPTY & we will not cut this node
+
             setposition(brd, cell[0], cell[1], EMPTY)
             alpha_cut_check = False
+
+            # But here if the alpha >= beta we will cut & we will rename our alpha,beta's
 
             if alpha >= beta:
                 alpha_cut = alpha
@@ -236,6 +299,8 @@ def MiniMax_AB(brd, depth, alpha, beta, player):
             return [row, col, beta, alpha, alpha_cut_check, alpha_cut, beta_cut]
 
 
+# Here we will use AI and choice library to choose a place set it , if all of our cells were full. (First Player)
+
 def AIMoveMiniMax(brd):
     if len(emptyCells(brd)) == 9:
         x = choice([0, 1, 2])
@@ -243,10 +308,14 @@ def AIMoveMiniMax(brd):
         setposition(brd, x, y, OPLAYER)
         showboard(brd)
 
+    # If not full we will run untill its over
+
     else:
         result = MiniMax_AB(brd, len(emptyCells(brd)), -inf, inf, OPLAYER)
         setposition(brd, result[0], result[1], OPLAYER)
         showboard(brd)
+
+# Here we will use AI and choice library to choose a place set it , if all of our cells were full in Alpha-Beta Purning. (First Player)
 
 
 def AIMoveAB(brd):
@@ -255,19 +324,23 @@ def AIMoveAB(brd):
         y = choice([0, 1, 2])
         setposition(brd, x, y, OPLAYER)
         showboard(brd)
-        print("alpha = -inf")
-        print("beta = inf")
+        print("Alpha = -infinity")
+        print("Beta = infinity")
+
+    # If not full we will run untill its over with fulling empty cells & it counts alpha,beta's cut
 
     else:
         result = MiniMax_AB(brd, len(emptyCells(brd)), -inf, inf, OPLAYER)
         setposition(brd, result[0], result[1], OPLAYER)
         showboard(brd)
-        print("alpha =", result[3])
-        print("beta =", result[2])
+        print("Alpha =", result[3])
+        print("Beta =", result[2])
         if result[4]:
-            print("alpha cut ", end="")
-            print("( alpha_cut =", result[5], end="")
-            print(", beta_cut =", result[6], ")")
+            print("Alpha cut ", end="")
+            print("( Alpha_cut =", result[5], end="")
+            print(", Beta_cut =", result[6], ")")
+
+# Here we will use AI and choice library to choose a place set it , if all of our cells were full in Alpha-Beta Purning but with one difference.
 
 
 def AIMoveABForSimple(brd):
@@ -276,19 +349,22 @@ def AIMoveABForSimple(brd):
         y = choice([0, 1, 2])
         setposition(brd, x, y, OPLAYER)
         showboard(brd)
-        print("alpha = -inf")
-        print("beta = inf")
+        print("Alpha = -infinity")
+        print("Beta = infinity")
 
+    # If not full we will run untill its over with final cell & it counts alpha,beta's cut
     else:
         result = MiniMax_AB(brd, 2, -inf, inf, OPLAYER)
         setposition(brd, result[0], result[1], OPLAYER)
         showboard(brd)
-        print("alpha =", result[3])
-        print("beta =", result[2])
+        print("Alpha =", result[3])
+        print("Beta =", result[2])
         if result[4]:
-            print("alpha cut ", end="")
-            print("( alpha_cut =", result[5], end="")
-            print(", beta_cut =", result[6], ")")
+            print("Alpha cut ", end="")
+            print("( Alpha_cut =", result[5], end="")
+            print(", Beta_cut =", result[6], ")")
+
+# Here we will use AI and choice library to choose a place set it , if all of our cells were full in Alpha-Beta Purning. (Secound Player)
 
 
 def AI2MoveAB(brd):
@@ -297,20 +373,24 @@ def AI2MoveAB(brd):
         y = choice([0, 1, 2])
         setposition(brd, x, y, XPLAYER)
         showboard(brd)
-        print("alpha = -inf")
-        print("beta = inf")
+        print("Alpha = -infinity")
+        print("Beta = infinity")
+
+    # If not full we will run untill its over with fulling empty cells & it counts alpha,beta's cut
 
     else:
         result = MiniMax_AB(brd, len(emptyCells(brd)), -inf, inf, XPLAYER)
         setposition(brd, result[0], result[1], XPLAYER)
         showboard(brd)
-        print("alpha =", result[2])
-        print("beta =", result[3])
+        print("Alpha =", result[2])
+        print("Beta =", result[3])
         if result[4]:
-            print("alpha cut ", end="")
-            print("( alpha_cut =", result[5], end="")
-            print(", beta_cut =", result[6], ")")
+            print("Alpha cut ", end="")
+            print("( Alpha_cut =", result[5], end="")
+            print(", Beta_cut =", result[6], ")")
 
+
+# If the user chooses AI vs Player , this will run & it wants to choose (AB vs AB) or (simple vs AB). it makes moves & sends each move for its def
 
 def bot():
     print("MiniMax vs MiniMax_AB or MiniMax_AB vs MiniMax_AB? (1/2)")
@@ -334,6 +414,8 @@ def bot():
 
         printResult(board)
 
+# Hard Move
+
 
 def makeMove(brd, player, mode):
     if mode == 1:
@@ -347,6 +429,8 @@ def makeMove(brd, player, mode):
             AIMoveAB(brd)
         else:
             AI2MoveAB(brd)
+
+# If the user enters mode 2 , this will makes (player vs AI) & If ther user enters 2 , this will makes (AI vs AI)
 
 
 def makeMoveForMiniMax(brd, player, mode):
@@ -362,6 +446,8 @@ def makeMoveForMiniMax(brd, player, mode):
         else:
             AI2MoveAB(brd)
 
+# Easy Move
+
 
 def makeMoveForSimple(brd, player, mode):
     if mode == 1:
@@ -370,6 +456,8 @@ def makeMoveForSimple(brd, player, mode):
 
         else:
             AIMoveABForSimple(brd)
+
+# In here User enters the diffculty between Hard or Easy & User enters to be first or second...
 
 
 def Pvbot():
@@ -390,12 +478,14 @@ def Pvbot():
                     Pvbot()
                 else:
                     break
+
+            # If wrong number entered
             except (KeyError, ValueError):
                 print(Fore.RED + "Info:: " + Fore.YELLOW +
                       "Please Enter a Number!!")
                 time.sleep(1)
                 Pvbot()
-
+        # Clear the board
         clearBoard(board)
         if order == 2:
             currentPlayer = OPLAYER
@@ -406,6 +496,7 @@ def Pvbot():
             makeMoveForSimple(board, currentPlayer, 1)
             currentPlayer *= -1
 
+        # Final Result
         printResult(board)
     else:
         while True:
@@ -421,13 +512,15 @@ def Pvbot():
                     Pvbot()
                 else:
                     break
+            # If wrong number entered
             except (KeyError, ValueError):
                 print(Fore.RED + "Info:: " + Fore.YELLOW +
                       "Please Enter a Number!!")
                 time.sleep(1)
                 Pvbot()
-
+        # Clear the board
         clearBoard(board)
+        # If user want to be second or first
         if order == 2:
             currentPlayer = OPLAYER
         else:
@@ -436,24 +529,5 @@ def Pvbot():
         while not (boardFull(board) or gameWon(board)):
             makeMove(board, currentPlayer, 1)
             currentPlayer *= -1
-
+        # Final Result
         printResult(board)
-
-# Welcome
-
-
-def run():
-    while True:
-        os.system("cls")
-        print(Fore.CYAN + "           TIC" + Fore.BLUE + "-" +
-              Fore.GREEN + "TAC" + Fore.BLUE + "-" + Fore.CYAN + "TOE" + "\nBy Bardia Fardar\n")
-        mode = input(
-            Fore.CYAN + "\n ┌─[AI vs AI or Player vs AI?]\n └──╼(1 or 2)>>> " + Fore.GREEN + "")
-        if int(mode) == 1:
-            bot()
-        else:
-            Pvbot()
-
-
-if __name__ == "__main__":
-    run()
