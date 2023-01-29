86111df76575360fd8c4b65404f2562c4f3cb70f
Barry Fardar <31126364+Bardiafa@users.noreply.github.com>
2023-01-29 18:08:48 +0330

    Upload

---
+++ Project Final - BardiaFardar.py
@@ -0,0 +1,459 @@
+# Bardia Fardar
+# Student Number 4003333028
+# AI Project - Final
+
+from colorama import Fore, Style
+from random import choice
+from math import inf
+import time
+import os
+
+
+left_down_angle = '\u2514'
+right_down_angle = '\u2518'
+right_up_angle = '\u2510'
+left_up_angle = '\u250C'
+
+middle_junction = '\u253C'
+top_junction = '\u252C'
+bottom_junction = '\u2534'
+right_junction = '\u2524'
+left_junction = '\u251C'
+
+
+bar = Style.BRIGHT + Fore.RED + '\u2502' + Fore.RESET + Style.RESET_ALL
+dash = '\u2500'
+
+first_line = Style.BRIGHT + Fore.RED + left_up_angle + dash + dash + dash + top_junction + dash + \
+    dash + dash + top_junction + dash + dash + dash + \
+    right_up_angle + Fore.RESET + Style.RESET_ALL
+middle_line = Style.BRIGHT + Fore.RED + left_junction + dash + dash + dash + middle_junction + dash + \
+    dash + dash + middle_junction + dash + dash + dash + \
+    right_junction + Fore.RESET + Style.RESET_ALL
+last_line = Style.BRIGHT + Fore.RED + left_down_angle + dash + dash + dash + bottom_junction + dash + \
+    dash + dash + bottom_junction + dash + dash + dash + \
+    right_down_angle + Fore.RESET + Style.RESET_ALL
+
+XPLAYER = 1
+OPLAYER = -1
+EMPTY = 0
+
+board = [[EMPTY, EMPTY, EMPTY],
+         [EMPTY, EMPTY, EMPTY],
+         [EMPTY, EMPTY, EMPTY]]
+
+
+def showboard(array):
+    print(first_line)
+    for a in range(len(array)):
+        for i in array[a]:
+            if i == EMPTY:
+                print(bar, ' ', end=' ')
+            else:
+                if i == 1:
+                    print(bar, "X", end=' ')
+                else:
+                    print(bar, "O", end=' ')
+        print(bar)
+        if a == 2:
+            print(last_line)
+        else:
+            print(middle_line)
+
+
+def clearBoard(brd):
+    for x, row in enumerate(brd):
+        for y, col in enumerate(row):
+            brd[x][y] = EMPTY
+
+
+def winningPlayer(brd, player):
+    winningStates = [[brd[0][0], brd[0][1], brd[0][2]],
+                     [brd[1][0], brd[1][1], brd[1][2]],
+                     [brd[2][0], brd[2][1], brd[2][2]],
+                     [brd[0][0], brd[1][0], brd[2][0]],
+                     [brd[0][1], brd[1][1], brd[2][1]],
+                     [brd[0][2], brd[1][2], brd[2][2]],
+                     [brd[0][0], brd[1][1], brd[2][2]],
+                     [brd[0][2], brd[1][1], brd[2][0]]]
+
+    if [player, player, player] in winningStates:
+        return True
+
+    return False
+
+
+def gameWon(brd):
+    return winningPlayer(brd, XPLAYER) or winningPlayer(brd, OPLAYER)
+
+
+def printResult(brd):
+    if winningPlayer(brd, XPLAYER):
+        print(Fore.GREEN + "😍 X won!" + '\n')
+        time.sleep(2)
+
+    elif winningPlayer(brd, OPLAYER):
+        print(Fore.GREEN + "😍 O won!" + '\n')
+        time.sleep(2)
+
+    else:
+        print(Fore.YELLOW + "😔 Tie No winner!" + '\n')
+        time.sleep(2)
+
+
+def emptyCells(brd):
+    emptyC = []
+    for x, row in enumerate(brd):
+        for y, col in enumerate(row):
+            if brd[x][y] == EMPTY:
+                emptyC.append([x, y])
+
+    return emptyC
+
+
+def boardFull(brd):
+    if len(emptyCells(brd)) == 0:
+        return True
+    return False
+
+
+def setposition(brd, x, y, player):
+    brd[x][y] = player
+
+
+def playerMove(brd):
+    e = True
+    moves = {1: [0, 0], 2: [0, 1], 3: [0, 2],
+             4: [1, 0], 5: [1, 1], 6: [1, 2],
+             7: [2, 0], 8: [2, 1], 9: [2, 2]}
+    while e:
+        try:
+            move = int(input(
+                Fore.CYAN + "\n ┌─[Insert Your position : ]\n └──╼(1-9)>>> " + Fore.YELLOW + ""))
+            if not move in range(1, 10):
+                print(Fore.RED + "Info:: " + Fore.YELLOW + "Invalid position!!")
+            elif not (moves[move] in emptyCells(brd)):
+                print(Fore.RED + "Info:: " + Fore.YELLOW + "Invalid position!!")
+            else:
+                os.system("cls")
+                setposition(brd, moves[move][0], moves[move][1], XPLAYER)
+                showboard(brd)
+                print("alpha = -inf")
+                print("beta = inf")
+                e = False
+        except (KeyError, ValueError):
+            print(Fore.RED + "Info:: " + Fore.YELLOW + "Please Enter a Number!!")
+
+
+def getScore(brd):
+    if winningPlayer(brd, XPLAYER):
+        return 1
+
+    elif winningPlayer(brd, OPLAYER):
+        return -1
+
+    else:
+        return 0
+
+
+def MiniMax(brd, depth, player):
+    row = -1
+    col = -1
+    best = 0
+
+    if player == XPLAYER:
+        best = -inf
+    else:
+        best = inf
+
+    if depth == 0 or gameWon(brd):
+        return [row, col, getScore(brd)]
+
+    else:
+        for cell in emptyCells(brd):
+            setposition(brd, cell[0], cell[1], player)
+            score = MiniMax(brd, depth - 1, -player)
+            if player == XPLAYER:
+
+                if score[2] > best:
+                    best = score[2]
+                    row = cell[0]
+                    col = cell[1]
+
+            else:
+                if score[2] < best:
+                    best = score[2]
+                    row = cell[0]
+                    col = cell[1]
+
+            setposition(brd, cell[0], cell[1], EMPTY)
+
+        if player == XPLAYER:
+            return [row, col]
+
+        else:
+            return [row, col]
+
+
+def MiniMax_AB(brd, depth, alpha, beta, player):
+    row = -1
+    col = -1
+    alpha_cut = 0
+    beta_cut = 0
+
+    if depth == 0 or gameWon(brd):
+        return [row, col, getScore(brd)]
+
+    else:
+        for cell in emptyCells(brd):
+            setposition(brd, cell[0], cell[1], player)
+            score = MiniMax_AB(brd, depth - 1, alpha, beta, -player)
+            if player == XPLAYER:
+                if score[2] > alpha:
+                    alpha = score[2]
+                    row = cell[0]
+                    col = cell[1]
+
+            else:
+                if score[2] < beta:
+                    beta = score[2]
+                    row = cell[0]
+                    col = cell[1]
+
+            setposition(brd, cell[0], cell[1], EMPTY)
+            alpha_cut_check = False
+
+            if alpha >= beta:
+                alpha_cut = alpha
+                beta_cut = beta
+                alpha_cut_check = True
+                break
+
+        if player == XPLAYER:
+            return [row, col, alpha, beta, alpha_cut_check, alpha_cut, beta_cut]
+
+        else:
+            return [row, col, beta, alpha, alpha_cut_check, alpha_cut, beta_cut]
+
+
+def AIMoveMiniMax(brd):
+    if len(emptyCells(brd)) == 9:
+        x = choice([0, 1, 2])
+        y = choice([0, 1, 2])
+        setposition(brd, x, y, OPLAYER)
+        showboard(brd)
+
+    else:
+        result = MiniMax_AB(brd, len(emptyCells(brd)), -inf, inf, OPLAYER)
+        setposition(brd, result[0], result[1], OPLAYER)
+        showboard(brd)
+
+
+def AIMoveAB(brd):
+    if len(emptyCells(brd)) == 9:
+        x = choice([0, 1, 2])
+        y = choice([0, 1, 2])
+        setposition(brd, x, y, OPLAYER)
+        showboard(brd)
+        print("alpha = -inf")
+        print("beta = inf")
+
+    else:
+        result = MiniMax_AB(brd, len(emptyCells(brd)), -inf, inf, OPLAYER)
+        setposition(brd, result[0], result[1], OPLAYER)
+        showboard(brd)
+        print("alpha =", result[3])
+        print("beta =", result[2])
+        if result[4]:
+            print("alpha cut ", end="")
+            print("( alpha_cut =", result[5], end="")
+            print(", beta_cut =", result[6], ")")
+
+
+def AIMoveABForSimple(brd):
+    if len(emptyCells(brd)) == 9:
+        x = choice([0, 1, 2])
+        y = choice([0, 1, 2])
+        setposition(brd, x, y, OPLAYER)
+        showboard(brd)
+        print("alpha = -inf")
+        print("beta = inf")
+
+    else:
+        result = MiniMax_AB(brd, 2, -inf, inf, OPLAYER)
+        setposition(brd, result[0], result[1], OPLAYER)
+        showboard(brd)
+        print("alpha =", result[3])
+        print("beta =", result[2])
+        if result[4]:
+            print("alpha cut ", end="")
+            print("( alpha_cut =", result[5], end="")
+            print(", beta_cut =", result[6], ")")
+
+
+def AI2MoveAB(brd):
+    if len(emptyCells(brd)) == 9:
+        x = choice([0, 1, 2])
+        y = choice([0, 1, 2])
+        setposition(brd, x, y, XPLAYER)
+        showboard(brd)
+        print("alpha = -inf")
+        print("beta = inf")
+
+    else:
+        result = MiniMax_AB(brd, len(emptyCells(brd)), -inf, inf, XPLAYER)
+        setposition(brd, result[0], result[1], XPLAYER)
+        showboard(brd)
+        print("alpha =", result[2])
+        print("beta =", result[3])
+        if result[4]:
+            print("alpha cut ", end="")
+            print("( alpha_cut =", result[5], end="")
+            print(", beta_cut =", result[6], ")")
+
+
+def bot():
+    print("MiniMax vs MiniMax_AB or MiniMax_AB vs MiniMax_AB? (1/2)")
+    mode = int(input())
+    if mode == 1:
+        currentPlayer = XPLAYER
+        clearBoard(board)
+
+        while not (boardFull(board) or gameWon(board)):
+            makeMoveForMiniMax(board, currentPlayer, 2)
+            currentPlayer *= -1
+
+        printResult(board)
+    else:
+        currentPlayer = XPLAYER
+        clearBoard(board)
+
+        while not (boardFull(board) or gameWon(board)):
+            makeMove(board, currentPlayer, 2)
+            currentPlayer *= -1
+
+        printResult(board)
+
+
+def makeMove(brd, player, mode):
+    if mode == 1:
+        if player == XPLAYER:
+            playerMove(brd)
+
+        else:
+            AIMoveAB(brd)
+    else:
+        if player == OPLAYER:
+            AIMoveAB(brd)
+        else:
+            AI2MoveAB(brd)
+
+
+def makeMoveForMiniMax(brd, player, mode):
+    if mode == 1:
+        if player == XPLAYER:
+            playerMove(brd)
+
+        else:
+            AIMoveMiniMax(brd)
+    else:
+        if player == OPLAYER:
+            AIMoveMiniMax(brd)
+        else:
+            AI2MoveAB(brd)
+
+
+def makeMoveForSimple(brd, player, mode):
+    if mode == 1:
+        if player == XPLAYER:
+            playerMove(brd)
+
+        else:
+            AIMoveABForSimple(brd)
+
+
+def Pvbot():
+    os.system("cls")
+    mode = int(input(
+        Fore.CYAN + "\n ┌─[Select diffculty , Easy or hard?]\n └──╼(1 or 2)>>> " + Fore.YELLOW + ""))
+    if mode == 1:
+        while True:
+            try:
+                os.system("cls")
+                order = int(input(
+                    Fore.CYAN + "\n ┌─[Do you want to play First or Second?]\n └──╼(1 or 2)>>> " + Fore.YELLOW + ""))
+                if not (order == 1 or order == 2):
+                    os.system("cls")
+                    print(Fore.RED + "Info:: " +
+                          Fore.YELLOW + "Please pick 1 or 2")
+                    time.sleep(1)
+                    Pvbot()
+                else:
+                    break
+            except (KeyError, ValueError):
+                print(Fore.RED + "Info:: " + Fore.YELLOW +
+                      "Please Enter a Number!!")
+                time.sleep(1)
+                Pvbot()
+
+        clearBoard(board)
+        if order == 2:
+            currentPlayer = OPLAYER
+        else:
+            currentPlayer = XPLAYER
+
+        while not (boardFull(board) or gameWon(board)):
+            makeMoveForSimple(board, currentPlayer, 1)
+            currentPlayer *= -1
+
+        printResult(board)
+    else:
+        while True:
+            try:
+                os.system("cls")
+                order = int(input(
+                    Fore.CYAN + "\n ┌─[Do you want to play First or Second?]\n └──╼(1 or 2)>>> " + Fore.YELLOW + ""))
+                if not (order == 1 or order == 2):
+                    os.system("cls")
+                    print(Fore.RED + "Info:: " +
+                          Fore.YELLOW + "Please pick 1 or 2")
+                    time.sleep(1)
+                    Pvbot()
+                else:
+                    break
+            except (KeyError, ValueError):
+                print(Fore.RED + "Info:: " + Fore.YELLOW +
+                      "Please Enter a Number!!")
+                time.sleep(1)
+                Pvbot()
+
+        clearBoard(board)
+        if order == 2:
+            currentPlayer = OPLAYER
+        else:
+            currentPlayer = XPLAYER
+
+        while not (boardFull(board) or gameWon(board)):
+            makeMove(board, currentPlayer, 1)
+            currentPlayer *= -1
+
+        printResult(board)
+
+# Welcome
+
+
+def run():
+    while True:
+        os.system("cls")
+        print(Fore.CYAN + "           TIC" + Fore.BLUE + "-" +
+              Fore.GREEN + "TAC" + Fore.BLUE + "-" + Fore.CYAN + "TOE" + "\nBy Bardia Fardar\n")
+        mode = input(
+            Fore.CYAN + "\n ┌─[AI vs AI or Player vs AI?]\n └──╼(1 or 2)>>> " + Fore.GREEN + "")
+        if int(mode) == 1:
+            bot()
+        else:
+            Pvbot()
+
+
+if __name__ == "__main__":
+    run()
--- README.md
+++ README.md
@@ -1,1 +1,154 @@
-# MiniMax-purning
+# tic-tac-toe-minimax
+ Tic-Tac-Toe game developed by Bardia Fardar
+
+# Introduction
+To solve games using AI, we will introduce the concept of a game tree followed by mini-max algorithm. The different states of the game are represented by nodes in the game tree.
+In the game tree, the nodes are sorted in levels that correspond to each player's turns in the game so that the “root” node of the tree  is the beginning position in the game. In tic-tac-toe, this would be the empty grid with no X`s or O`s played yet. Under root node, on the second level, there are the possible states that can result from the first player’s moves, be it X or O. We call these nodes the “children” of the root node. Each node on the second level, would further have as its children nodes the states that can be reached from it by the opposing player's moves. This is continued, level by level, until reaching state where the game is over.
+
+# What is Mini-max?
+Minimax is a decision rule used in artificial intelligence, decision theory, game theory, statistics, and philosophy for minimizing the possible loss for a worst case (maximum loss) scenario. When dealing with gains, it is referred to as "maximin" – to maximize the minimum gain.
+
+# How does it works?
+In game theory, minimax is a decision rule used to minimize the worst-case potential loss; in other words, a player considers all of the best opponent responses to his strategies, and selects the strategy such that the opponent's best strategy gives a payoff as large as possible.
+
+
+# Understanding the Algorithm
+
+```python
+
+def MiniMaxAB(brd, depth, alpha, beta, player):
+    row = -1
+    collumn = -1
+    alpha_cut = 0
+    beta_cut = 0
+
+    if depth == 0 or gameWon(brd):
+        return [row, collumn, getScore(brd)]
+
+    else:
+        for cell in emptyCells(brd):
+            setposition(brd, cell[0], cell[1], player)
+            score = MiniMaxAB(brd, depth - 1, alpha, beta, -player)
+            if player == XPLAYER:
+                if score[2] > alpha:
+                    alpha = score[2]
+                    row = cell[0]
+                    collumn = cell[1]
+
+            else:
+                if score[2] < beta:
+                    beta = score[2]
+                    row = cell[0]
+                    collumn = cell[1]
+
+            setposition(brd, cell[0], cell[1], EMPTY)
+            alpha_cut_check = False
+
+            if alpha >= beta:
+                alpha_cut = alpha
+                beta_cut = beta
+                alpha_cut_check = True
+                break
+
+        if player == XPLAYER:
+            return [row, collumn, alpha, beta, alpha_cut_check, alpha_cut, beta_cut]
+
+        else:
+            return [row, collumn, beta, alpha, alpha_cut_check, alpha_cut, beta_cut]
+
+```
+
+Now we'll see each part of this pseudocode with Python implementation. The Python implementation is available at this repository. First of all, consider it:
+> ┌───┬───┬───┐
+> │ O │ X │ O │
+> ├───┼───┼───┤
+> │ X │   │   │
+> ├───┼───┼───┤
+> │   │   │   │
+> └───┴───┴───┘
+
+> alpha = -inf
+> beta = inf
+
+>  ┌─[Insert Your position : ]
+   └──╼(1-9)>>>
+
+The Alpha may be X or O and the Beta may be O or X, whatever. The board is 3x3.
+
+```python
+def MiniMaxAB(brd, depth, alpha, beta, player):
+```
+* **brd**: the current board in tic-tac-toe (node)
+* **depth**: index of the node in the game tree
+* **alpha**: maximum
+* **beta**: minimum
+* **player**: may be a *alpha* player or *beta* player
+
+```python
+if alpha >= beta:
+    alpha_cut = alpha
+    beta_cut = beta
+    alpha_cut_check = True
+    break
+```
+
+Both players start with your worst score. If player is Alpha, its score is -infinity. Else if player is Beta, its score is +infinity. 
+
+The best move on the board is [-1, -1] (row and column) for all.
+
+```python
+if depth == 0 or gameWon(brd):
+    return [row, col, getScore(brd)]
+```
+
+If the depth is == 0, then the board hasn't new empty cells to play. Or, if a player wins, then the game ended for MAX or MIN. So the score for that state will be returned.
+
+* If Alpha won: return +1
+* If Beta won: return -1
+* Else: return 0 (Draw)
+
+Now we'll see the main part of this code that contains recursion.
+
+```python
+def emptyCells(brd):
+    emptyC = []
+    for x, row in enumerate(brd):
+        for y, col in enumerate(row):
+            if brd[x][y] == EMPTY:
+                emptyC.append([x, y])
+
+    return emptyC
+```
+
+For each valid moves (empty cells):
+* **x**: receives cell row index
+* **y**: receives cell column index
+* **brd[x][y]**: it's like board[available_row][available_col] receives MAX or MIN player
+* **score = minimax(brd, depth - 1, -player)**:
+  * brd: is the current board in recursion;
+  * depth -1: index of the next state;
+  * -player: if a player is MAX (+1) will be MIN (-1) and vice versa.
+
+The move (+1 or -1) on the board is undo and the row, column are collected.
+
+The next step is compare the score with best.
+
+```python
+if player == XPLAYER:
+	if score[2] > best[2]:
+		best = score[2]
+        row = cell[0]
+        col = cell[1]
+else:
+	if score[2] < best[2]:
+		best = score[2]
+        row = cell[0]
+        col = cell[1]
+```
+
+For XPLAYER player, a bigger score will be received. For a YPlayer, a lower score will be received.
+
+# Evaluation Function
+
+An evaluation function, also known as a heuristic evaluation function or static evaluation function, is a function used by game-playing programs to estimate the value or goodness of a position in the minimax and related algorithms. The evaluation function is typically designed to prioritize speed over accuracy; the function looks only at the current position and does not explore possible moves (therefore static).
+
